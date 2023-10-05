2206ea814d3d62496ccda9ebbb6dc0c299f5b630
Pereyro <petrgubko@gmail.com>
2023-10-05 22:37:13 +0300

    add database create function

--- src/app/routes/auth.py
+++ src/app/routes/auth.py
@@ -1,12 +1,20 @@
 from flask import request, Blueprint
+from ..data import connect_to_database
 
 auth_bp = Blueprint('auth_bp', __name__)
 
 
 @auth_bp.route('/', methods=['GET', 'POST'])
 def index():
-    print('aaaa')
-    return {}
+    conn = connect_to_database("metadata.db")
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+        conn.close()
+        return {"users": users}, 200
+    else:
+        return {"error": "Unable to connect to the database"}, 500
 
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
